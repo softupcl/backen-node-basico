9ac66762c7e4e4c01543f18175be7fe9bd922c6b
Luis Urzua <lurzua@softup.cl>
2021-05-28 19:20:40 -0400

    backend basico listo

---
+++ controllers/usuarios.js
@@ -0,0 +1,45 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req = request, res = response) => {
+
+    const { q, nombre } = req.query;
+    res.json({
+        "msg": "get API - controlador",
+        q,
+        nombre
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+    res.json({
+        "msg": "post API - controlador",
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id;
+    res.json({
+        "msg": "put API - controlador",
+        id
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        "msg": "delete API - controlador"
+    });
+}
+
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
